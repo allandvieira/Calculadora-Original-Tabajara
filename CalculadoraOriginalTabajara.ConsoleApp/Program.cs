a079163ac2b2945be12e8472a4fb9298d74f5e05
Allan <Allan@DESKTOP-RA9V1AJ>
2024-03-15 15:23:06 -0300

    Add project files.

---
+++ CalculadoraOriginalTabajara.ConsoleApp/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace CalculadoraOriginalTabajara.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                // Titulo da calculadora
+                Console.WriteLine("Calculadora Original Tabajara em C#\r");
+                Console.WriteLine("-----------------------------------\n");
+
+                // Menu
+                Console.WriteLine("Escolha uma operação matemática no Menu abaixo:");
+                Console.WriteLine("\tDigite 1 para Adicionar");
+                Console.WriteLine("\tDigite 2 para Subtrair");
+                Console.WriteLine("\tDigite 3 para Multiplicar");
+                Console.WriteLine("\tDigite 4 para Dividir");
+                Console.WriteLine("\tDigite S para sair\n");
+
+                Console.WriteLine("Qual a sua opção?\n");
+
+                string operacao = Console.ReadLine();
+
+                // || = comparação não obrigatória (só um precisa ser verdadeiro) 
+                if (operacao == "S" || operacao == "s")
+                    break;
+
+                // && = comparação obrigatória
+                if (operacao != "1" && operacao != "2" && operacao != "3" && operacao != "4" && operacao != "S" && operacao != "s")
+                {
+                    Console.WriteLine("\nOperação inválida, digite qualquer tecla e tente novamente...\n");
+                    Console.ReadLine();
+
+                    continue;
+                }
+
+                // Entrada do primeiro numero
+                Console.WriteLine("\nDigite o primeiro número:");
+
+                string primeiroNumeroString = "";
+
+                primeiroNumeroString = Console.ReadLine();
+
+                double primeiroNumero = Convert.ToDouble(primeiroNumeroString);
+
+                // Entrada do segundo numero
+                Console.WriteLine("\nDigite o segundo número:");
+
+                string segundoNumeroString = Console.ReadLine();
+
+                double segundoNumero = Convert.ToDouble(segundoNumeroString);
+
+                double resultado = 0;
+
+                if (operacao == "1")
+                {
+                    resultado = primeiroNumero + segundoNumero;
+                }
+
+                else if (operacao == "2")
+                {
+                    resultado = primeiroNumero - segundoNumero;
+                }
+
+                else if (operacao == "3")
+                {
+                    resultado = primeiroNumero * segundoNumero;
+                }
+
+                else if (operacao == "4")
+                {
+                    // Verifique se o segundo número é 0
+
+                    while (segundoNumero == 0)
+                    {
+                        // caso for 0, pedir para o usuário digitar de novo
+                        Console.WriteLine("\nSegundo número não pode ser ZERO, tente novamente!");
+
+                        Console.ReadLine();
+
+                        Console.WriteLine("\nDigite o segundo número novamente: ");
+
+                        segundoNumero = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                    // Caso não seja 0, divida o número
+                    resultado = primeiroNumero / segundoNumero;
+                }
+
+                Console.WriteLine("\nO resultado é: " + resultado);
+
+                // Resposta do usuário antes de fechar
+                Console.WriteLine("\nPressione 'S' e Enter para fechar o aplicativo ou pressione qualquer outra tecla e Enter para continuar:\n");
+                string operacao2 = Console.ReadLine();
+
+                // || = comparação não obrigatória (só um precisa ser verdadeiro) 
+                if (operacao2 == "S" || operacao2 == "s")
+                    break;
+                else
+                {
+                    continue;
+                }
+
+            }
+        }
+    }
+}
